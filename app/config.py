f0f7614cb4d9dd4c95dc0a0fd52232989b750d6e
Emma <emmanuelibbk@gmail.com>
2025-10-08 10:43:35 +0100

    ddS

--- app/config.py
+++ app/config.py
@@ -15,6 +15,9 @@ class Settings(BaseSettings):
     CELERY_BROKER_URL: str = "redis://localhost:6379/0"
     CELERY_RESULT_BACKEND: str = "redis://localhost:6379/0"
 
+    resend_api_key: str
+    resend_app_url: str
+
     MONNIFY_ENVIRONMENT: str = "sandbox"
     MONNIFY_API_KEY: str
     MONNIFY_SECRET_KEY: str
