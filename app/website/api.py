beaee937e6eabcc4c37394bbaab41016457171a2
Emma <emmanuelibbk@gmail.com>
2025-10-28 04:52:02 +0100

    dud

--- app/website/api.py
+++ app/website/api.py
@@ -1537,8 +1537,8 @@ async def verify_payment(account_reference: str, background_tasks: BackgroundTas
         paid_transaction = None
         expected_amount = float(payment_session["amount"])
         cutoff_time = datetime.utcnow() - timedelta(minutes=5)
-        session_created_time = datetime.fromisoformat(payment_session["created_at"].replace("+01:00", "").replace("Z", ""))
-
+        session_created_time_str = payment_session["created_at"].replace("+01:00", "+00:00")
+        session_created_time = datetime.fromisoformat(session_created_time_str).replace(tzinfo=None)
         
         
         for txn in content:
